1c521460b54b249efa4334149a6ad91ea869e5d6
Martin Petersen <petersen.martin.imd@gmail.com>
2020-02-21 21:08:03 -0300

    Alteração na classe Main para testar as demais modificações feitas no programa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,29 @@ public class Main {
 
         System.out.println(conta1.getSaldo());
         System.out.println(conta2.getSaldo());
+        System.out.println();
+
         System.out.println(conta1.toString());
+        System.out.println(conta2.toString());
+        System.out.println();
 
         System.out.println(Conta.getTotalContas());
+        System.out.println();
 
-        Funcionario novoFuncionario = new Funcionario("Maria", 22, "728.266.840-00", 2500, "bancaria");
+        Bancario novoBancario = new Bancario("Maria Nercy", 22, "728.266.840-00", 2500, "bancaria", 3, "1668-7");
+        Gerente novoGerente = new Gerente("Martin Petersen", 23, "004.236.460-44", 5000, "gerente", 3, "1856-0");
+        Diretor novoDiretor = new Diretor("Lionel Richie", 70, "687.563.350-75", 30000, "diretor", 2, "sul");
 
-        System.out.println(novoFuncionario.toString());
+        System.out.println(novoBancario.toString());
+        System.out.println(novoBancario.getSalario() + novoBancario.bonusFuncionario());
+        System.out.println();
+
+        System.out.println(novoGerente.toString());
+        System.out.println(novoGerente.getSalario() + novoGerente.bonusFuncionario());
+        System.out.println();
+
+        System.out.println(novoDiretor.toString());
+        System.out.println(novoDiretor.getSalario() + novoDiretor.bonusFuncionario());
+
     }
 }
